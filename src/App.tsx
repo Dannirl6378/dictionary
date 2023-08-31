383ea9b8600be57ae8b5006848fcc46326d48a90
Daniel <allprogsandprojects.@gmail.com>
2023-08-31 18:41:03 +0200

    feat: basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,56 @@
-import React from 'react';
+import React, { useState } from "react";
+import axios from "axios";
+
+interface DictionaryData {
+  word:string;
+}
 
 function App() {
+  const [word, setWord] = useState(``);
+  const [data, setData] = useState<DictionaryData | null>(null);
+  const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+
+  const getword = (event: { key: string }) => {
+    if (event.key === "Enter") {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+      setWord(``);
+    }
+  };
+
   return (
-    <div className="App">
+    <div className="container">
+      <div className="head">
+        <div className="bar">
+          <h1>Search Bar</h1>
+          <p>For dictionary</p>
+          <div className="search">
+            <input
+              value={word}
+              onChange={(event) => setWord(event.target.value)}
+              onKeyDown={getword}
+              placeholder="Enter word"
+              type="text"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="body">
+        <div className="word">
+          <h2>Word: {data?.word} </h2>
+        </div>
+        <div className="definition">
+          <p>Definition/Meaning:</p>
+        </div>
+        <div className="example">
+          <p>Example:</p>
+        </div>
+        <div className="synonym">
+          <p>Synonym:</p>
+        </div>
+      </div>
     </div>
   );
 }
